3a662cfb0a77fb34213172036e21f0bab3732013
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-10-07 11:05:32 +0200

    add new create recipe route

---
+++ backend/src/routes/addNewRecipe.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('../../db/mongoose');
+const Recipe = require('../../models/recipe');
+const Ingredient = require('../../models/ingredient');
+
+const router = express.Router();
+
+connectDB();
+
+router.post('/recipes', async (req, res) => {
+  try {
+    const { recipeName, isVegan, isVegetarian, numberOfSpoon, instructionPoints, difficultyRate, yummyRating, bakingTime, createdBy, ingredients } = req.body;
+
+    const ingredientPromises = ingredients.map(async ing => {
+      const ingredient = await Ingredient.findOneAndUpdate(
+        { name: ing.name, unityType: ing.unityType },
+        { name: ing.name, unityType: ing.unityType, ingType: ing.ingType },
+        { upsert: true, new: true }
+      );
+      return { ingredient: ingredient._id, qty: ing.qty };
+    });
+
+    const ingredientRefs = await Promise.all(ingredientPromises);
+
+
+    const newRecipe = new Recipe({
+      recipeName,
+      isVegan,
+      isVegetarian,
+      numberOfSpoon,
+      instructionPoints,
+      difficultyRate,
+      yummyRating,
+      bakingTime,
+      createdBy,
+      ingredients: ingredientRefs
+    });
+
+    await newRecipe.save();
+    res.status(201).json(newRecipe);
+  } catch (err) {
+    console.error('Error saving recipe:', err);
+    res.status(500).json({ message: 'Server error' });
+  } finally {
+    mongoose.connection.close();
+  }
+});
+
+module.exports = router;
