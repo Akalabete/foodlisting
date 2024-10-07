a41d10aab87e77b68071284fc84b995cf11de340
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-10-07 09:06:00 +0200

    swap the db model / connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./db/mongoose');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use('/api', require('./src/routes/index'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(bodyParser.json());
+
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
